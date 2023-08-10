c0304c9957ad353608c69fb58f16333bde489c59
Desthy <desthywork@macbook-air-de-desthy.home>
2023-08-10 18:32:15 +0200

    suite

--- wp-content/themes/planty/header.php
+++ wp-content/themes/planty/header.php
@@ -3,14 +3,24 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>" />
 <meta name="viewport" content="width=device-width" />
+<link href="https://fonts.googleapis.com/" type="text/css" rel="stylesheet"> 
 <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
-
 <header id="header" role="banner">
+    <div class="container">
+        <a href="index.php"><img src="./img/logo.png" class="logo" alt="logo planty"></a> 
+        <nav>
+            <a href="#">Nous rencontrer</a>
+            <a href="#">Admin</a>
+        </nav>
+        <button class="nav">Commander</button>
+    </div>
 
-<h3>mon header</h3>
+   
+
+
 </header>
 <div id="container">
 <main id="content" role="main">
